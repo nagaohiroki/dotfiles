72957f8d762500f1ec478ec36172acb724d64456
nagao_hiroki <nagao_hiroki@hand.co.jp>
2015-10-08 10:13:39 +0900

    Unity関係もの更新

--- .vim/omni_build.bat
+++ .vim/omni_build.bat
@@ -1,12 +1,9 @@
-set DOT_VIM=%VIM%/.vim
+set DOT_VIM=%VIM%\.vim
 
 cd %VIM%/bundle/omnisharp-vim
-git submodule foreach git clean
 git submodule foreach git pull origin master
-patch -f -p0 < "%DOT_VIM%/0001-Corresponding-to-Japanese.patch"
 
 cd server
-git submodule foreach git clean
 git submodule foreach git pull origin master
 msbuild
 copy "%DOT_VIM%\config.json" "OmniSharp/bin/Debug"
