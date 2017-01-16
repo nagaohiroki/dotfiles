760e466d02f9e90baecffb05ce300f04dc5bfc3c
nagao_hiroki <zerosiseinokae@yahoo.co.jp>
2017-01-16 13:47:58 +0900

    fix pep8 warning old_rev.py

--- .vim/old_rev.py
+++ .vim/old_rev.py
@@ -1,9 +1,10 @@
-﻿# coding:utf-8
+#!/usr/bin/python2
+# coding:utf-8
 from __future__ import print_function
 from __future__ import division
 from __future__ import unicode_literals
 from __future__ import absolute_import
-from xml.etree.ElementTree import *
+from xml.etree.ElementTree import xt
 import sys
 import os
 import subprocess
@@ -15,9 +16,10 @@ try:
 except ImportError:
     is_vim_import = False
 
+
 def svn_add_revfile(file_path):
     out = subprocess.check_output('svn log --quiet --xml ' + file_path)
-    elem = fromstring(out)
+    elem = xt.fromstring(out)
     logentry = elem.findall('logentry')
     mini = sys.maxint
     for l in logentry:
@@ -25,13 +27,20 @@ def svn_add_revfile(file_path):
 
     comd = 'svn cat -r ' + str(mini) + ' ' + file_path
     result = subprocess.check_output(comd)
-    old_file_path = os.environ['TEMP'] + '/' + 'r' + str(mini) + '_' + file_path
-    old_file = codecs.open(old_file_path, 'w' , 'utf-8')
+    temp = os.environ['TEMP']
+    old_file_path = temp + '/' + 'r' + str(mini) + '_' + file_path
+    old_file = codecs.open(old_file_path, 'w', 'utf-8')
     old_file.write(result.decode('utf-8'))
     old_file.close()
     return old_file_path
 
+
+def main():
+    if not is_vim_import:
+        return
+    old_file_path = svn_add_revfile(vim.eval("expand('%')"))
+    vim.command("VDsplit " + old_file_path)
+
+
 if __name__ == '__main__':
-    if is_vim_import == True:
-        old_file_path = svn_add_revfile(vim.eval("expand('%')"))
-        vim.command("VDsplit " + old_file_path)
+    main()
