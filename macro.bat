df57c611c509a0ab7ffa0182da7ce21b989334be
nagaohiroki <zerosiseinokae@yahoo.co.jp>
2017-02-11 10:32:49 +0900

    update windows macro.bat

--- macro.bat
+++ macro.bat
@@ -1,14 +1,15 @@
 @echo off
 doskey ls=dir /w /d /b $*
-doskey ll=dir /ad /d /b $*
 doskey cp=copy $*
 doskey pwd=echo %CD%
 doskey mv=move $*
 doskey tree=tree /f $*
 doskey cat=type $*
+doskey cd=cd /d $*
 doskey h=doskey /history $*
 doskey history=doskey /history $*
-doskey ex=explorer %CD%
+doskey ex=explorer $*
+doskey excd=explorer %CD%
 doskey vi=gvim $*
 doskey vim=gvim $*
 doskey gvim=gvim $*
