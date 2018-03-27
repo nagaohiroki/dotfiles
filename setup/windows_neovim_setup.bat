0b7413857ae9bfa8f76ddaa85b6546d9760332da
nagaohiroki <testplayers0226@gmail.com>
2018-03-27 16:39:22 +0900

    add test

--- setup/windows_neovim_setup.bat
+++ setup/windows_neovim_setup.bat
@@ -1,4 +1,3 @@
 mkdir "%USERPROFILE%\AppData\Local\nvim"
 mklink "%USERPROFILE%\AppData\Local\nvim\init.vim"  "%USERPROFILE%\dotfiles\.vimrc"
-mklink "%USERPROFILE%\AppData\Local\nvim\ginit.vim"  "%USERPROFILE%\dotfiles\.gvimrc"
 pause
