87887911fd76a9563157f3f72a82515a77f56c7a
nagao_hiroki <zerosiseinokae@yahoo.co.jp>
2019-07-29 21:51:48 +0900

    delete unused plugin

--- vsopen.bat
+++ vsopen.bat
@@ -1,2 +1,1 @@
-"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" /edit "%1"
-rem "D:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.exe" /edit "%1"
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.exe" /edit "%1" /command "edit.goto %2"
