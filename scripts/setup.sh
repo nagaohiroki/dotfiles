32a79117e11984e938cecdabd297bc851510e1d1
nagaohiroki <testplayers0226@gmail.com>
2021-10-22 13:40:45 +0900

    change ff

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ln -s "~/dotfiles/.vimrc" "~/.vimrc"
 ln -s "~/dotfiles/.gvimrc" "~/.gvimrc"
-ln -s "~/dotfiles/nvim" "~/.config/nvim"
+ln -s "~/dotfiles/nvim/init.vim" "~/.config/nvim/init.vim"
