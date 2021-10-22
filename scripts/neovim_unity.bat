e8a07cce2045846cb091ff8283d283e1f68ba004
nagao_hiroki <zerosiseinokae@yahoo.co.jp>
2021-10-22 13:42:50 +0900

    add windows unity bat

---
+++ scripts/neovim_unity.bat
@@ -0,0 +1,2 @@
+D:\nvim-win64\Neovim\bin\nvim-qt.exe %1 %2
+rem $(File) +$(Line)
--- scripts/ycm_build_win.bat
+++
@@ -1,2 +0,0 @@
-cd /d %USERPROFILE%\vim-plug\YouCompleteMe
-python install.py --cs-completer --go-completer --clangd-completer --msvc 15
