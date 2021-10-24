2d23968b4d3b77fda31097699a654f04a97026a2
nagaohiroki <testplayers0226@gmail.com>
2021-10-24 09:37:21 +0900

    nvim

--- scripts/neovim_open.py
+++ scripts/neovim_open.py
@@ -5,28 +5,40 @@ import sys
 import platform
 
 
-def func(address, cmd):
+def neovim_command(address):
+    cmd = sys.argv
+    cmd[0] = 'edit'
+    edit = ' '.join(cmd)
     nvim = neovim.attach('socket', path=address)
-    nvim.command(cmd)
-    nvim.close()
+    nvim.command(edit)
+
+
+def open_neovim():
+    cmd = sys.argv
+    if platform.system() == 'Windows':
+        cmd[0] = 'nvim-qt'
+    if platform.system() == 'Darwin':
+        cmd[0] = '/Applications/goneovim.app/Contents/MacOS/goneovim'
+    subprocess.Popen(cmd)
+
+
+def activate_neovim():
+    if platform.system() == 'Darwin':
+        subprocess.Popen(['open', '-a', 'goneovim'])
 
 
 def main():
     env = os.path.join(os.path.expanduser('~'), '.cache_neovim', 'env.txt')
     if not os.path.exists(env):
+        open_neovim()
         return
     f = open(env)
     address = f.readlines()[1]
-    cmd = sys.argv;
     try:
-        cmd[0] = 'edit'
-        func(address, ' '.join(cmd))
+        neovim_command(address)
+        activate_neovim()
     except:
-        if platform.system() == 'Windows':
-            cmd[0] = 'nvim-qt'
-        if platform.system() == 'Darwin':
-            cmd = ['open', '-a', 'goneovim'] + cmd[1:]
-        subprocess.Popen(cmd)
+        open_neovim()
 
 
 if __name__ == "__main__":
