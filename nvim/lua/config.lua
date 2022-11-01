d61648ec53f2455ff567e02077bee92fd81f0c06
nagaohiroki <testplayers0226@gmail.com>
2022-11-01 14:59:12 +0900

    fix lsp

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -1,78 +1,80 @@
 require('telescope').setup
 {
-  defaults = {preview = false}
+	defaults = { preview = false }
 }
 
-local lspinstaller = require("nvim-lsp-installer")
-lspinstaller.setup{}
+require("nvim-lsp-installer").setup {}
 local lspconfig = require("lspconfig")
-lspconfig.omnisharp.setup{use_mono = true}
-lspconfig.pyright.setup{}
-lspconfig.clangd.setup{}
-local cmp = require'cmp'
+lspconfig.omnisharp.setup { use_mono = true }
+lspconfig.pyright.setup {}
+lspconfig.clangd.setup {}
+lspconfig.sumneko_lua.setup {}
+lspconfig.vimls.setup {}
+lspconfig.jsonls.setup {}
+local cmp = require 'cmp'
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      vim.fn["vsnip#anonymous"](args.body)
-    end,
-  },
-  mapping = {
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm(),
-    ['<Tab>'] = cmp.mapping.select_next_item(),
-    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-    ['<Down>'] = cmp.mapping.select_next_item(),
-    ['<Up>'] = cmp.mapping.select_prev_item(),
-  },
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'vsnip' },
-    { name = 'buffer' },
-    { name = 'nvim_lsp_signature_help' },
-  }
+	snippet = {
+		expand = function(args)
+			vim.fn["vsnip#anonymous"](args.body)
+		end,
+	},
+	mapping = {
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<CR>'] = cmp.mapping.confirm(),
+		['<Tab>'] = cmp.mapping.select_next_item(),
+		['<S-Tab>'] = cmp.mapping.select_prev_item(),
+		['<Down>'] = cmp.mapping.select_next_item(),
+		['<Up>'] = cmp.mapping.select_prev_item(),
+	},
+	sources = {
+		{ name = 'nvim_lsp' },
+		{ name = 'vsnip' },
+		{ name = 'buffer' },
+		{ name = 'nvim_lsp_signature_help' },
+	}
 })
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = "all",
-  ignore_install = {"phpdoc", "fortran", "haskell", "rnoweb", "markdown"},
-  highlight = {
-    enable = true,
-  },
+require 'nvim-treesitter.configs'.setup {
+	ensure_installed = "all",
+	ignore_install = { "phpdoc", "fortran", "haskell", "rnoweb", "markdown" },
+	highlight = {
+		enable = true,
+	},
 }
 local dap = require('dap')
 dap.adapters.python = {
-  type = 'executable';
-  command = 'python';
-  args = { '-m', 'debugpy.adapter' };
+	type = 'executable';
+	command = 'python';
+	args = { '-m', 'debugpy.adapter' };
 }
 dap.configurations.python = {
-  {
-    type = 'python';
-    request = 'launch';
-    name = "Launch file";
-    program = "${file}";
-    pythonPath = function()
-      return 'python'
-    end;
-  },
+	{
+		type = 'python';
+		request = 'launch';
+		name = "Launch file";
+		program = "${file}";
+		pythonPath = function()
+			return 'python'
+		end;
+	},
 }
 local unityDebugCommand = vim.env.HOME .. '/.vscode/extensions/deitry.unity-debug-3.0.11/bin/UnityDebug.exe'
 local unityDebugArgs = {}
 if vim.fn.has('win32') == 0 then
-  unityDebugArgs = {unityDebugCommand}
-  unityDebugCommand = 'mono'
+	unityDebugArgs = { unityDebugCommand }
+	unityDebugCommand = 'mono'
 end
 dap.adapters.unity = {
-  type = 'executable';
-  command = unityDebugCommand;
-  args = unityDebugArgs;
-  name = 'Unity Editor';
+	type = 'executable';
+	command = unityDebugCommand;
+	args = unityDebugArgs;
+	name = 'Unity Editor';
 }
 dap.configurations.cs = {
-  {
-    type = 'unity';
-    request = 'launch';
-    name = "Unity Editor";
-    path = "Library/EditorInstance.json";
-  },
+	{
+		type = 'unity';
+		request = 'launch';
+		name = "Unity Editor";
+		path = "Library/EditorInstance.json";
+	},
 }
 require("dapui").setup()
