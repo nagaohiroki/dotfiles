3f7ad40ddf1c051c3b4c5469cfe94dcab665e683
nagaohiroki <testplayers0226@gmail.com>
2022-11-09 10:57:32 +0900

    supoprt plugin install

--- nvim/init.lua
+++ nvim/init.lua
@@ -25,9 +25,113 @@ if my_servername ~= vim.v.servername then
 		vim.cmd.exit()
 	end
 end
+vim.g.mapleader = " "
+vim.o.writebackup = false
+vim.o.fixeol = false
+vim.o.wrap = false
+vim.o.swapfile = false
+vim.o.smartindent = true
+vim.o.smartcase = true
+vim.o.ignorecase = true
+vim.o.title = true
+vim.o.number = true
+vim.o.list = true
+vim.o.undofile = true
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.cmdheight = 2
+vim.o.mouse = 'a'
+vim.o.listchars = 'eol:<,tab:> ,extends:<'
+vim.o.whichwrap = 'b,s,h,l,<,>,[,]'
+vim.o.clipboard = 'unnamedplus,unnamed'
+vim.o.fileencodings = 'ucs-bom,iso-2022-jp-3,euc-jisx0213,cp932,sjis,euc-jp,utf-8'
+vim.o.statusline = '%<%f%m%r%h%w%y[%{&fenc}%{(&bomb?"bom":"")}]%=%c,%l/%L'
+vim.keymap.set({ 'n', 'v' }, '<C-p>', '"0p')
+vim.keymap.set('n', '<leader>s', [[:%s/\<<C-R><C-W>\>//g<Left><Left>]])
+vim.api.nvim_create_user_command('Rc', function() vim.cmd([[e ]] .. vim.env.HOME .. [[/dotfiles/nvim/init.lua]]) end, {})
+vim.api.nvim_create_user_command('CdCurrent', function() vim.api.nvim_set_current_dir(vim.fn.expand('%:p:h')) end, {})
+vim.api.nvim_create_user_command('CopyPath', function() vim.fn.setreg('*', vim.fn.expand('%:p')) end, {})
+vim.api.nvim_create_user_command('CopyPathLine',
+	function() vim.fn.setreg('*', vim.fn.expand('%:p') .. '#L' .. vim.fn.line('.')) end, {})
+vim.api.nvim_create_user_command('Wex',
+	function()
+		local file = vim.api.nvim_buf_get_name(0)
+		if vim.fn.has('mac') then
+			vim.fn.system('open ' .. file)
+		end
+		if vim.fn.has('win32') then
+			vim.fn.system('start explorer /select,' .. file)
+		end
+	end, {})
 
-vim.cmd [[packadd packer.nvim]]
-require 'packer'.startup(function(use)
+function FontSize(inc)
+	if vim.g.GuiLoaded ~= 1 then
+		return
+	end
+	vim.g.fontSize = math.max(1, vim.g.fontSize + inc)
+	vim.api.nvim_command('Guifont! ' .. vim.g.fontName .. ':h' .. vim.g.fontSize)
+end
+
+vim.keymap.set('n', '+', function() FontSize(1) end)
+vim.keymap.set('n', '-', function() FontSize(-1) end)
+vim.api.nvim_create_augroup('loading', {})
+vim.api.nvim_create_autocmd("UIEnter", {
+	group = 'loading',
+	once = true,
+	callback = function()
+		if vim.g.GuiLoaded ~= 1 then
+			return
+		end
+		if vim.g.server_mode == 1 then
+			vim.cmd.exit()
+			return
+		end
+		vim.api.nvim_command('GuiWindowOpacity 0.95')
+		vim.api.nvim_command('GuiScrollBar 1')
+		if vim.fn.has('mac') then
+			vim.g.fontName = [[Monaco]]
+			vim.g.fontSize = 13
+		end
+		if vim.fn.has('win32') == 1 then
+			vim.g.fontName = [[Migu 1M]]
+			vim.g.fontSize = 12
+		end
+		FontSize(0)
+	end
+})
+vim.api.nvim_create_autocmd('QuickFixCmdPost',
+	{
+		group = 'loading',
+		command = 'cwindow'
+	})
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' },
+	{
+		group = 'loading',
+		pattern = { '*.usf', '*.ush', '*.cginc', '*.shader', '*.glslinc' },
+		command = 'setfiletype hlsl'
+	})
+vim.api.nvim_create_autocmd('BufRead',
+	{
+		group = 'loading',
+		callback = function()
+			local line = vim.fn.line("'\"")
+			if line > 0 and line <= vim.fn.line("$") then
+				vim.fn.cursor(line, vim.fn.col("'\""))
+			end
+		end
+	})
+vim.api.nvim_create_user_command('PackerInit', function()
+	vim.fn.system('git clone https://github.com/wbthomason/packer.nvim "' ..
+		vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim"')
+end, {})
+-- plugins
+vim.cmd [[silent! packadd packer.nvim]]
+local ok, packer = pcall(require, 'packer')
+if not ok then
+	print('cannot install plugins Please :PackerInit')
+	return
+end
+packer.startup(function(use)
 	use 'wbthomason/packer.nvim'
 	use 'beyondmarc/hlsl.vim'
 	use 'cohama/agit.vim'
@@ -61,8 +165,7 @@ require 'packer'.startup(function(use)
 	use 'rcarriga/nvim-dap-ui'
 	use 'voldikss/vim-translator'
 end)
-vim.g.mapleader = " "
-vim.api.nvim_command [[colorscheme iceberg]]
+vim.api.nvim_command [[silent! colorscheme iceberg]]
 -- telescope
 require('telescope').setup { defaults = { preview = false } }
 local builtin = require('telescope.builtin')
@@ -83,10 +186,6 @@ vim.keymap.set('n', '<leader>l', vim.lsp.buf.document_symbol)
 vim.keymap.set('n', '<leader>e', vim.lsp.buf.declaration)
 vim.keymap.set('i', '<C-s>', Vsnip, { expr = true })
 vim.api.nvim_create_user_command('Format', function() vim.lsp.buf.format { async = true } end, {})
-vim.api.nvim_create_user_command('PackerInit', function()
-	vim.fn.system('git clone https://github.com/wbthomason/packer.nvim "' ..
-		vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim"')
-end, {})
 require("nvim-lsp-installer").setup {}
 local lspconfig = require("lspconfig")
 lspconfig.omnisharp.setup { use_mono = true }
@@ -202,98 +301,3 @@ vim.keymap.set('n', '<leader>d', ':SignifyDiff<CR>')
 -- VS
 vim.keymap.set('n', '<leader>b', ':VSBreakPoint<CR>')
 vim.keymap.set('n', '<leader>v', ':VSOpen<CR>')
--- standard options
-vim.o.writebackup = false
-vim.o.fixeol = false
-vim.o.wrap = false
-vim.o.swapfile = false
-vim.o.smartindent = true
-vim.o.smartcase = true
-vim.o.ignorecase = true
-vim.o.title = true
-vim.o.number = true
-vim.o.list = true
-vim.o.undofile = true
-vim.o.shiftwidth = 4
-vim.o.tabstop = 4
-vim.o.cmdheight = 2
-vim.o.mouse = 'a'
-vim.o.listchars = 'eol:<,tab:> ,extends:<'
-vim.o.whichwrap = 'b,s,h,l,<,>,[,]'
-vim.o.clipboard = 'unnamedplus,unnamed'
-vim.o.fileencodings = 'ucs-bom,iso-2022-jp-3,euc-jisx0213,cp932,sjis,euc-jp,utf-8'
-vim.o.statusline = '%<%f%m%r%h%w%y[%{&fenc}%{(&bomb?"bom":"")}]%=%c,%l/%L'
-vim.keymap.set({ 'n', 'v' }, '<C-p>', '"0p')
-vim.keymap.set('n', '<leader>s', [[:%s/\<<C-R><C-W>\>//g<Left><Left>]])
-vim.api.nvim_create_user_command('Rc', function() vim.cmd([[e ]] .. vim.env.HOME .. [[/dotfiles/nvim/init.lua]]) end, {})
-vim.api.nvim_create_user_command('CdCurrent', function() vim.api.nvim_set_current_dir(vim.fn.expand('%:p:h')) end, {})
-vim.api.nvim_create_user_command('CopyPath', function() vim.fn.setreg('*', vim.fn.expand('%:p')) end, {})
-vim.api.nvim_create_user_command('CopyPathLine',
-	function() vim.fn.setreg('*', vim.fn.expand('%:p') .. '#L' .. vim.fn.line('.')) end, {})
-vim.api.nvim_create_user_command('Wex',
-	function()
-		local file = vim.api.nvim_buf_get_name(0)
-		if vim.fn.has('mac') then
-			vim.fn.system('open ' .. file)
-		end
-		if vim.fn.has('win32') then
-			vim.fn.system('start explorer /select,' .. file)
-		end
-	end, {})
-
-function FontSize(inc)
-	if vim.g.GuiLoaded ~= 1 then
-		return
-	end
-	vim.g.fontSize = math.max(1, vim.g.fontSize + inc)
-	vim.api.nvim_command('Guifont! ' .. vim.g.fontName .. ':h' .. vim.g.fontSize)
-end
-
-vim.keymap.set('n', '+', function() FontSize(1) end)
-vim.keymap.set('n', '-', function() FontSize(-1) end)
-vim.api.nvim_create_augroup('loading', {})
-vim.api.nvim_create_autocmd("UIEnter", {
-	group = 'loading',
-	once = true,
-	callback = function()
-		if vim.g.GuiLoaded ~= 1 then
-			return
-		end
-		if vim.g.server_mode == 1 then
-			vim.cmd.exit()
-			return
-		end
-		vim.api.nvim_command('GuiWindowOpacity 0.95')
-		vim.api.nvim_command('GuiScrollBar 1')
-		if vim.fn.has('mac') then
-			vim.g.fontName = [[Monaco]]
-			vim.g.fontSize = 13
-		end
-		if vim.fn.has('win32') == 1 then
-			vim.g.fontName = [[Migu 1M]]
-			vim.g.fontSize = 12
-		end
-		FontSize(0)
-	end
-})
-vim.api.nvim_create_autocmd('QuickFixCmdPost',
-	{
-		group = 'loading',
-		command = 'cwindow'
-	})
-vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' },
-	{
-		group = 'loading',
-		pattern = { '*.usf', '*.ush', '*.cginc', '*.shader', '*.glslinc' },
-		command = 'setfiletype hlsl'
-	})
-vim.api.nvim_create_autocmd('BufRead',
-	{
-		group = 'loading',
-		callback = function()
-			local line = vim.fn.line("'\"")
-			if line > 0 and line <= vim.fn.line("$") then
-				vim.fn.cursor(line, vim.fn.col("'\""))
-			end
-		end
-	})
