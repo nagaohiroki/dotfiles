899c1f2663c1a06b651a8b378d4651d02ffbe5f7
nagaohiroki <testplayers0226@gmail.com>
2022-12-06 17:47:29 +0900

    id -> -FriendlyName

--- scripts/reconnect_google_nest_audio.ps1
+++ scripts/reconnect_google_nest_audio.ps1
@@ -1,4 +1,4 @@
-$id = 'BTHENUM\{0000110E-0000-1000-8000-00805F9B34FB}_VID&0001000F_PID&1200\7&2CDD7520&0&14C14EC70853_C00000000'
-Disable-PnpDevice -InstanceId $id -Confirm:$false
+$dev  = Get-PnpDevice -FriendlyName "google nest audio Stereo"
+Disable-PnpDevice -InstanceId $dev.instanceId -Confirm:$false
 Start-Sleep -Seconds 10
-Enable-PnpDevice -InstanceId $id -Confirm:$false
+Enable-PnpDevice -InstanceId $dev.instanceId -Confirm:$false
