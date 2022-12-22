bf33d7e531ad399d2f7d68526bacd6eda71dc4fe
nagaohiroki <testplayers0226@gmail.com>
2022-12-22 22:36:11 +0900

    move Dockerfile

--- scripts/reconnect_google_nest_audio.bat
+++
@@ -1,1 +0,0 @@
-pwsh -Command "Start-Process pwsh -Verb runas reconnect_google_nest_audio.ps1"
--- scripts/reconnect_google_nest_audio.ps1
+++
@@ -1,4 +0,0 @@
-$dev  = Get-PnpDevice -FriendlyName "google nest audio Stereo"
-Disable-PnpDevice -InstanceId $dev.instanceId -Confirm:$false
-Start-Sleep -Seconds 10
-Enable-PnpDevice -InstanceId $dev.instanceId -Confirm:$false
