4a2b216aad2267e4e1299bd08e87a80ef1f00f3b
nagaohiroki <testplayers0226@gmail.com>
2023-12-25 17:52:55 +0900

    update GenerateProject

--- scripts/GenerateProject.py
+++ scripts/GenerateProject.py
@@ -4,24 +4,48 @@ import json
 
 
 def generate_project():
-    with open(os.path.join(os.path.expanduser("~"), "ue_config.json"), "r") as f:
+    ue_config = os.path.join(os.path.expanduser("~"), "ue_config.json")
+    if not os.path.exists(ue_config):
+        config_dict = {
+            "project": "C:\\work\\yourprojectdir",
+            "engine": "C:\\Program Files\\Epic Games\\UE_5.3",
+        }
+        with open(ue_config, "w") as f:
+            json.dump(config_dict, f, indent=4)
+        print("Please edit " + ue_config)
+        return
+    with open(ue_config, "r") as f:
         config = json.load(f)
-    project = config["project"]
     engine = config["engine"]
+    if not os.path.exists(engine):
+        print("not found " + engine + " in " + ue_config)
+        return
+    project_dir = config["project"]
+    if not os.path.exists(project_dir):
+        print("not found " + project_dir + " in " + ue_config)
+        return
+    project_name = os.path.basename(project_dir)
+    uproject = os.path.join(project_dir, project_name + ".uproject")
     bat = os.path.join(engine, "Engine", "Build", "BatchFiles", "Build.bat")
     subprocess.call(
-        [bat, "-projectfiles", "-project=" + project, "-game", "-rocket", "-progress"],
+        [
+            bat,
+            "-projectfiles",
+            "-project=" + uproject,
+            "-game",
+            "-rocket",
+            "-progress",
+        ],
         shell=True,
     )
-    prj, _ = os.path.splitext(os.path.basename(project))
     subprocess.call(
         [
             bat,
             "-mode=GenerateClangDatabase",
             "-game",
             "-engine",
-            "-project=" + project,
-            prj + "Editor",
+            "-project=" + uproject,
+            project_name + "Editor",
             "Development",
             "Win64",
         ],
@@ -29,7 +53,7 @@ def generate_project():
     )
     database = "compile_commands.json"
     src = os.path.join(engine, database)
-    dst = os.path.join(os.path.dirname(project), database)
+    dst = os.path.join(project_dir, database)
     os.replace(src, dst)
 
 
