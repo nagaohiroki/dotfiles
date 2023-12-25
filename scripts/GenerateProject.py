e56d00dc6757763341835cb158342d6310e3041f
nagaohiroki <testplayers0226@gmail.com>
2023-12-25 10:31:19 +0900

    add ue config

--- scripts/GenerateProject.py
+++ scripts/GenerateProject.py
@@ -4,35 +4,33 @@ import json
 
 
 def generate_project():
-    with open(os.path.join(os.path.expanduser('~'), 'ue_config.json'), 'r') as f:
+    with open(os.path.join(os.path.expanduser("~"), "ue_config.json"), "r") as f:
         config = json.load(f)
-    engine = config['engine']
-    project = config['project']
-    bat = os.path.join(engine, 'Engine', 'Build', 'BatchFiles', 'Build.bat')
-    subprocess.call([
-            bat,
-            '-projectfiles',
-            '-project=' + project,
-            '-game',
-            '-rocket',
-            '-progress'
-            ], shell=True)
+    project = config["project"]
+    engine = config["engine"]
+    bat = os.path.join(engine, "Engine", "Build", "BatchFiles", "Build.bat")
+    subprocess.call(
+        [bat, "-projectfiles", "-project=" + project, "-game", "-rocket", "-progress"],
+        shell=True,
+    )
     prj, _ = os.path.splitext(os.path.basename(project))
-    subprocess.call([
+    subprocess.call(
+        [
             bat,
-            '-mode=GenerateClangDatabase',
-            '-game',
-            '-engine',
-            '-project=' + project,
-            prj + 'Editor',
-            'Development',
-            'Win64',
-            ], shell=True)
-    database = 'compile_commands.json'
-    with open(os.path.join(engine, database), 'r') as reader:
-        with open(os.path.join(os.path.dirname(project), database), 'w') as writer:
-            read = reader.read().replace('-std=', '/std:')
-            writer.write(read)
+            "-mode=GenerateClangDatabase",
+            "-game",
+            "-engine",
+            "-project=" + project,
+            prj + "Editor",
+            "Development",
+            "Win64",
+        ],
+        shell=True,
+    )
+    database = "compile_commands.json"
+    src = os.path.join(engine, database)
+    dst = os.path.join(os.path.dirname(project), database)
+    os.replace(src, dst)
 
 
 if __name__ == "__main__":
