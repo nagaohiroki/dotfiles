5e6b821d2def7ef1250dc569c18e961b9d1b5074
nagaohiroki <testplayers0226@gmail.com>
2024-09-20 10:33:34 +0900

    add semtic token config

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -78,64 +78,50 @@
 	{
 		'seblj/roslyn.nvim',
 		ft = 'cs',
-		config = function()
-			require('roslyn').setup(
-				{
-					on_attach = function(client)
-						-- NOTE: Super hacky... Don't know if I like that we set a random variable on the client
-						-- Seems to work though
-						if client.is_hacked then
-							return
-						end
-						client.is_hacked = true
-
-						-- let the runtime know the server can do semanticTokens/full now
-						client.server_capabilities = vim.tbl_deep_extend("force", client.server_capabilities, {
-							semanticTokensProvider = {
-								full = true,
-							},
-						})
-
-						-- monkey patch the request proxy
-						local request_inner = client.request
-						client.request = function(method, params, handler, req_bufnr)
-							if method ~= vim.lsp.protocol.Methods.textDocument_semanticTokens_full then
-								return request_inner(method, params, handler)
-							end
-
-							local function find_buf_by_uri(search_uri)
-								local bufs = vim.api.nvim_list_bufs()
-								for _, buf in ipairs(bufs) do
-									local name = vim.api.nvim_buf_get_name(buf)
-									local uri = "file://" .. name
-									if uri == search_uri then
-										return buf
-									end
-								end
-							end
-
-							local target_bufnr = find_buf_by_uri(params.textDocument.uri)
-							local line_count = vim.api.nvim_buf_line_count(target_bufnr)
-							local last_line = vim.api.nvim_buf_get_lines(target_bufnr, line_count - 1, line_count, true)
-								[1]
-
-							return request_inner("textDocument/semanticTokens/range", {
-								textDocument = params.textDocument,
-								range = {
-									["start"] = {
-										line = 0,
-										character = 0,
-									},
-									["end"] = {
-										line = line_count - 1,
-										character = string.len(last_line) - 1,
-									},
-								},
-							}, handler, req_bufnr)
-						end
+		opts = {
+			config = {
+				on_attach = function(client)
+					-- NOTE: Super hacky... Don't know if I like that we set a random variable on the client
+					-- Seems to work though
+					if client.is_hacked then
+						return
 					end
-				})
-		end
+					client.is_hacked = true
+
+					-- let the runtime know the server can do semanticTokens/full now
+					client.server_capabilities = vim.tbl_deep_extend("force", client.server_capabilities, {
+						semanticTokensProvider = {
+							full = true,
+						},
+					})
+
+					-- monkey patch the request proxy
+					local request_inner = client.request
+					client.request = function(method, params, handler, req_bufnr)
+						if method ~= vim.lsp.protocol.Methods.textDocument_semanticTokens_full then
+							return request_inner(method, params, handler)
+						end
+
+						local target_bufnr = vim.uri_to_bufnr(params.textDocument.uri)
+						local line_count = vim.api.nvim_buf_line_count(target_bufnr)
+						local last_line = vim.api.nvim_buf_get_lines(target_bufnr, line_count - 1, line_count, true)[1]
+						return request_inner("textDocument/semanticTokens/range", {
+							textDocument = params.textDocument,
+							range = {
+								["start"] = {
+									line = 0,
+									character = 0,
+								},
+								["end"] = {
+									line = line_count - 1,
+									character = string.len(last_line) - 1,
+								},
+							},
+						}, handler, req_bufnr)
+					end
+				end
+			}
+		},
 	},
 	{
 		'neovim/nvim-lspconfig',
