9a492cb2d8ef5b602c2a7840caac0ea24c5bed92
nagaohiroki <testplayers0226@gmail.com>
2024-10-02 11:29:40 +0900

    fix error

--- nvim/lua/singleton.lua
+++ nvim/lua/singleton.lua
@@ -1,4 +1,5 @@
-function CheckSingleton()
+local M = {}
+M.singleton = function()
 	local my_server = vim.fn.has('win32') == 1 and
 		[[\\.\pipe\nvim-server]] or vim.env.HOME .. [[/.local/state/nvim/nvim226]]
 	if my_server == vim.v.servername then
@@ -17,7 +18,7 @@ function CheckSingleton()
 			once = true,
 			callback = function()
 				local cmd = string.format(
-					'"%s" --server "%s" --remote-send "<Esc>:lua require(\'singleton\').editFile([[%s]],%s,%s)<CR>"',
+					'"%s" --server "%s" --remote-send "<Esc>:lua require(\'singleton\').edit_file([[%s]],%s,%s)<CR>"',
 					vim.v.progpath, my_server, vim.api.nvim_buf_get_name(0), vim.fn.line('.'), vim.fn.col('.'))
 				vim.cmd('enew')
 				vim.fn.jobstart(cmd, { on_exit = function() vim.cmd('q') end })
@@ -26,23 +27,16 @@ function CheckSingleton()
 	return true
 end
 
-function Foreground()
+M.edit_file = function(fname, line, col)
+	if fname == '' then
+		vim.cmd('enew')
+	else
+		vim.cmd('drop ' .. vim.fn.fnameescape(fname))
+		vim.fn.cursor(line, col)
+	end
 	if vim.g.GuiLoaded == 1 then
 		vim.cmd('py3file ' .. vim.env.HOME .. '/dotfiles/scripts/foreground.py')
 	end
 end
 
-function EditFile(fname, line, col)
-	if fname == '' then
-		vim.cmd('enew')
-	else
-		vim.cmd('edit ' .. fname)
-		vim.fn.cursor(line, col)
-	end
-	Foreground()
-end
-
-return {
-	singleton = CheckSingleton,
-	editFile = EditFile
-}
+return M
