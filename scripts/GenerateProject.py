b360df772acdbd0d5cfa687a116aac29eb9e7a49
nagaohiroki <testplayers0226@gmail.com>
2024-10-20 14:12:28 +0900

    genereate ue5 project files

--- scripts/GenerateProject.py
+++ scripts/GenerateProject.py
@@ -1,61 +1,65 @@
-import subprocess
+﻿import subprocess
 import os
 import json
+import glob
 
 
-def generate_project():
-    ue_config = os.path.join(os.path.expanduser("~"), "ue_config.json")
-    if not os.path.exists(ue_config):
-        config_dict = {
-            "project": "C:\\work\\yourprojectdir",
-            "engine": "C:\\Program Files\\Epic Games\\UE_5.3",
-        }
-        with open(ue_config, "w") as f:
-            json.dump(config_dict, f, indent=4)
-        print("Please edit " + ue_config)
-        return
-    with open(ue_config, "r") as f:
-        config = json.load(f)
-    engine = config["engine"]
-    if not os.path.exists(engine):
-        print("not found " + engine + " in " + ue_config)
-        return
-    project_dir = config["project"]
-    if not os.path.exists(project_dir):
-        print("not found " + project_dir + " in " + ue_config)
-        return
-    project_name = os.path.basename(project_dir)
-    uproject = os.path.join(project_dir, project_name + ".uproject")
+def generate_project(engine, uproject):
+    pj_dir = os.path.dirname(uproject)
+    name = os.path.basename(pj_dir)
     bat = os.path.join(engine, "Engine", "Build", "BatchFiles", "Build.bat")
-    subprocess.call(
-        [
-            bat,
-            "-projectfiles",
-            "-project=" + uproject,
-            "-game",
-            "-rocket",
-            "-progress",
-        ],
-        shell=True,
+    sln = [bat, "-projectfiles", f"-project={uproject}", "-game", "-engine"]
+    database = [
+        bat,
+        "-mode=GenerateClangDatabase",
+        "-game",
+        "-engine",
+        f"-project={uproject}",
+        f"{name}Editor",
+        "Development",
+        "Win64",
+        f"-OutputDir={pj_dir}",
+    ]
+    subprocess.call(sln, shell=True)
+    subprocess.call(database, shell=True)
+
+
+def find_uproject(cur=os.getcwd()):
+    uproject = glob.glob(os.path.join(cur, "*.uproject"))
+    if len(uproject) > 0:
+        return uproject[0]
+    else:
+        new_cur = os.path.dirname(cur)
+        if new_cur == cur:
+            return None
+        return find_uproject(new_cur)
+
+
+def find_ue_path():
+    gameuser = os.path.join(
+        os.environ["LOCALAPPDATA"],
+        "EpicGamesLauncher",
+        "Saved",
+        "Config",
+        "Windows",
+        "GameUserSettings.ini",
     )
-    subprocess.call(
-        [
-            bat,
-            "-mode=GenerateClangDatabase",
-            "-game",
-            "-engine",
-            "-project=" + uproject,
-            project_name + "Editor",
-            "Development",
-            "Win64",
-        ],
-        shell=True,
-    )
-    database = "compile_commands.json"
-    src = os.path.join(engine, database)
-    dst = os.path.join(project_dir, database)
-    os.replace(src, dst)
+    with open(gameuser, "r") as f:
+        for line in f:
+            install_dir = "DefaultAppInstallLocation="
+            if line.startswith(install_dir):
+                return line.replace(install_dir, "").replace("\n", "")
+
+
+def main():
+    uproject = find_uproject()
+    with open(uproject, "r") as f:
+        dic = json.load(f)
+        ver = dic["EngineAssociation"]
+        engine_dir = find_ue_path()
+        engine = os.path.join(engine_dir, f"UE_{ver}")
+        generate_project(engine, uproject)
 
 
 if __name__ == "__main__":
-    generate_project()
+    main()
