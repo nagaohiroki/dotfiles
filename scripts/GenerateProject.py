78ad1941fcc15baa540c418570eb241506585932
nagaohiroki <testplayers0226@gmail.com>
2024-10-26 12:19:41 +0900

    update unreal engine build script

--- scripts/GenerateProject.py
+++ scripts/GenerateProject.py
@@ -1,13 +1,11 @@
 ﻿import subprocess
 import os
 import json
-import glob
+import pathlib
 
 
-def generate_project(engine, uproject):
-    pj_dir = os.path.dirname(uproject)
-    name = os.path.basename(pj_dir)
-    bat = os.path.join(engine, "Engine", "Build", "BatchFiles", "Build.bat")
+def build(engine, uproject):
+    bat = str(engine.joinpath("Engine", "Build", "BatchFiles", "Build.bat"))
     sln = [bat, "-projectfiles", f"-project={uproject}", "-game", "-engine"]
     database = [
         bat,
@@ -15,28 +13,27 @@ def generate_project(engine, uproject):
         "-game",
         "-engine",
         f"-project={uproject}",
-        f"{name}Editor",
+        f"{uproject.stem}Editor",
         "Development",
         "Win64",
-        f"-OutputDir={pj_dir}",
+        f"-OutputDir={uproject.parent}",
     ]
     subprocess.call(sln, shell=True)
     subprocess.call(database, shell=True)
 
 
-def find_uproject(cur=os.getcwd()):
-    uproject = glob.glob(os.path.join(cur, "*.uproject"))
+def find_uproject_path(cur: pathlib.Path):
+    uproject = list(cur.glob("*.uproject"))
     if len(uproject) > 0:
         return uproject[0]
-    else:
-        new_cur = os.path.dirname(cur)
-        if new_cur == cur:
-            return None
-        return find_uproject(new_cur)
+    new_cur = cur.parent
+    if new_cur == cur:
+        return None
+    return find_uproject_path(new_cur)
 
 
 def find_ue_path():
-    gameuser = os.path.join(
+    gameuser = pathlib.Path(
         os.environ["LOCALAPPDATA"],
         "EpicGamesLauncher",
         "Saved",
@@ -48,21 +45,21 @@ def find_ue_path():
         for line in f:
             install_dir = "DefaultAppInstallLocation="
             if line.startswith(install_dir):
-                return line.replace(install_dir, "").replace("\n", "")
+                ue_path = line.replace(install_dir, "").replace("\n", "")
+                return pathlib.Path(ue_path)
 
 
 def main():
-    uproject = find_uproject()
+    uproject = find_uproject_path(pathlib.Path.cwd())
     if uproject is None:
         return
     with open(uproject, "r") as f:
         dic = json.load(f)
         ver = dic["EngineAssociation"]
-        engine_dir = find_ue_path()
-        if engine_dir is None:
+        engine = find_ue_path()
+        if engine is None:
             return
-        engine = os.path.join(engine_dir, f"UE_{ver}")
-        generate_project(engine, uproject)
+        build(engine.joinpath(f"UE_{ver}"), uproject)
 
 
 if __name__ == "__main__":
