dda1bb3a0590e527f252d31d631029d3d93778c0
nagaohiroki <testplayers0226@gmail.com>
2024-11-01 07:59:57 +0900

    delete dependencies UnityAdapterProbe

--- nvim/lua/unity.lua
+++ nvim/lua/unity.lua
@@ -43,44 +43,17 @@ local function unity_message_port()
 	end
 	return debugger_port + 2
 end
-local function find_probe()
-	local vstuc_path = vim.fn.fnameescape(vim.fn.stdpath('data') .. '/vstuc/extension/bin')
-	local system_obj = vim.system({ 'dotnet', vstuc_path .. '/UnityAttachProbe.dll' }, { text = true })
-	local probe_result = system_obj:wait(2000).stdout
-	if probe_result == nil or #probe_result == 0 then
-		print('No endpoint found (is unity running?)')
-		return nil
-	end
-	for json in vim.gsplit(probe_result, '\n') do
-		if json ~= '' then
-			local probe = vim.json.decode(json)
-			for _, p in pairs(probe) do
-				if p.isBackground == false then
-					return p
-				end
-			end
-		end
-	end
-	return nil
-end
 local function request(tbl)
-	local probe = find_probe()
-	if probe == nil then
-		vim.print('not find unity endpoint')
+	local messager_port = unity_message_port()
+	if messager_port == nil then
 		return
 	end
-	-- not required UnityAttachProbe.dll. but it's slow
-	-- local messager_port = unity_message_port()
-	-- if messager_port == nil then
-	-- 	return
-	-- end
 	local uv = vim.uv
 	local udp = uv.new_udp()
 	local json = vim.fn.json_encode(tbl)
-	-- uv.udp_send(udp, json, '127.0.0.1', messager_port, function(err)
-	uv.udp_send(udp, json, probe.address, probe.messagerPort, function(err)
+	uv.udp_send(udp, json, '127.0.0.1', messager_port, function(err)
 		if err then
-			print('error:', err)
+			vim.print('error:', err)
 		else
 			uv.close(udp)
 		end
