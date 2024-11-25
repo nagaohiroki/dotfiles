9b1d026fe0a195a5accfb5b80ec3ae48b1b38ac8
nagaohiroki <testplayers0226@gmail.com>
2024-11-26 08:00:47 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -2,25 +2,21 @@ import os
 import sys
 import platform
 import subprocess
+import pathlib
 
 
 def make_symlink():
-    home = os.path.expanduser("~")
+    home = pathlib.Path.home()
     nvim = "nvim"
-    dotfiles = os.path.join(home, "dotfiles")
-    nvim_src = os.path.join(dotfiles, nvim)
-    nvim_dst = os.path.join(home, ".config", nvim)
+    nvim_src = home.joinpath("dotfiles").joinpath(nvim)
+    nvim_dst = home.joinpath(".config", nvim)
     if platform.system() == "Windows":
-        nvim_dst = os.path.join(os.environ["LOCALAPPDATA"], nvim)
+        nvim_dst = pathlib.Path(os.environ["LOCALAPPDATA"], nvim)
     symlink(nvim_src, nvim_dst)
-    omnisharp = ".omnisharp"
-    omni_src = os.path.join(dotfiles, "scripts", omnisharp)
-    omni_dst = os.path.join(home, omnisharp)
-    symlink(omni_src, omni_dst)
 
 
 def symlink(src, dst):
-    if os.path.exists(dst):
+    if dst.exists():
         os.remove(dst)
         print(f"remove {dst}")
     os.symlink(src, dst, target_is_directory=True)
@@ -28,21 +24,12 @@ def symlink(src, dst):
 
 
 def install_requirements():
+    requirements = pathlib.Path.home().joinpath("dotfiles", "requirements.txt")
     subprocess.run(
-        [
-            sys.executable,
-            "-m",
-            "pip",
-            "install",
-            "--upgrade",
-            "-r",
-            os.path.join(
-                os.path.expanduser("~"), "dotfiles", "scripts", "requirements.txt"
-            ),
-        ]
+        [sys.executable, "-m", "pip", "install", "--upgrade", "-r", requirements]
     )
 
 
 if __name__ == "__main__":
-    make_symlink()
     install_requirements()
+    make_symlink()
