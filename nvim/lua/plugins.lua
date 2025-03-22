17ed5ee0ab482fbf967669d70424c164f98050cd
nagaohiroki <testplayers0226@gmail.com>
2025-03-22 12:24:51 +0900

    ちょっと変更

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -142,11 +142,7 @@ return {
       local mason_lspconfig = require('mason-lspconfig')
       local lspconfig = require('lspconfig')
       mason_lspconfig.setup({ ensure_installed = { 'lua_ls', 'pyright', 'clangd', 'marksman', 'jsonls' } })
-      mason_lspconfig.setup_handlers({
-        function(server_name)
-          lspconfig[server_name].setup({})
-        end
-      })
+      mason_lspconfig.setup_handlers({ function(server_name) lspconfig[server_name].setup({}) end })
     end
   },
   {
