41875a965c5c50df9e065a23c5e973db8bf54667
nagaohiroki <testplayers0226@gmail.com>
2025-05-09 10:20:43 +0900

    change wait message

--- nvim/lua/singleton.lua
+++ nvim/lua/singleton.lua
@@ -20,20 +20,28 @@ local function restore_session()
   end
   vim.fn.mkdir(session_dir, 'p')
   vim.cmd('mksession! ' .. session .. ' | enew')
-  vim.api.nvim_buf_set_lines(vim.api.nvim_get_current_buf(), 0, -1, false, { 'wait for open...' })
-  local cmd = { vim.v.progpath, '--server', vim.g.NVIM_SINGLETON, '--remote-send', '<Esc>:source ' .. session .. '<CR>' }
-  vim.system(cmd, nil, function() vim.schedule(function() vim.cmd('q!') end) end)
+  vim.api.nvim_buf_set_lines(vim.api.nvim_get_current_buf(), 0, -1, false, {
+    'target server: ' .. vim.g.NVIM_SINGLETON,
+    'mksession: ' .. session,
+    'please wait...'
+  })
+  vim.system({
+    vim.v.progpath,
+    '--server',
+    vim.g.NVIM_SINGLETON,
+    '--remote-send',
+    '<Esc>:source ' .. session .. '<CR>'
+  }, nil, function() vim.schedule(function() vim.cmd('q!') end) end)
 end
 local function open_other_server()
   vim.opt.swapfile = false
   vim.opt.writebackup = false
   vim.opt.loadplugins = false
   vim.opt.shada = ''
-  vim.api.nvim_create_autocmd('VimEnter',
-    {
-      once = true,
-      callback = function() restore_session() end
-    })
+  vim.api.nvim_create_autocmd('VimEnter', {
+    once = true,
+    callback = function() restore_session() end
+  })
 end
 local M = {}
 M.singleton = function()
