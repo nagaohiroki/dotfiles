818509022f4a5dfd82dee7dd6952bd478cd8b5d7
nagaohiroki <testplayers0226@gmail.com>
2025-08-13 10:41:15 +0900

    update singleton.lua

--- nvim/lua/singleton.lua
+++ nvim/lua/singleton.lua
@@ -6,24 +6,19 @@ local function is_running()
 end
 local function restore()
   vim.cmd('wshada')
-  vim.api.nvim_buf_set_lines(vim.api.nvim_get_current_buf(), 0, -1, false, {
-    'server: ' .. vim.g.NVIM_SINGLETON,
-    'please wait...'
-  })
+  vim.api.nvim_buf_set_lines(vim.api.nvim_get_current_buf(), 0, -1, false,
+    { 'server: ' .. vim.g.NVIM_SINGLETON, 'please wait...' })
   vim.system({ vim.v.progpath, '--server', vim.g.NVIM_SINGLETON, '--remote-send', '<Esc>:rshada | normal! `0<CR>' },
     nil, function() vim.schedule(function() vim.cmd('qa!') end) end)
-end
-local function open_other_server()
-  vim.opt.swapfile = false
-  vim.opt.writebackup = false
-  vim.opt.loadplugins = false
-  vim.opt.shadafile = ''
-  vim.api.nvim_create_autocmd('VimEnter', { once = true, callback = restore })
 end
 function M.setup()
   vim.cmd('rshada')
   if is_running() then
-    open_other_server()
+    vim.opt.swapfile = false
+    vim.opt.writebackup = false
+    vim.opt.loadplugins = false
+    vim.opt.shadafile = ''
+    vim.api.nvim_create_autocmd('VimEnter', { once = true, callback = restore })
     return true
   end
   vim.g.NVIM_SINGLETON = vim.v.servername
