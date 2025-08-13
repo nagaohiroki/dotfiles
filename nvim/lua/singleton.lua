fd65d6d7cc61cdbeb5fb29db9f207d4c667a0004
nagaohiroki <testplayers0226@gmail.com>
2025-08-13 10:34:23 +0900

    change singleton.lua

--- nvim/lua/singleton.lua
+++ nvim/lua/singleton.lua
@@ -1,51 +1,27 @@
 local M = {}
-M._config = {
-  session = {
-    dir = vim.fs.joinpath(vim.fn.stdpath('data'), 'singleton'),
-    save_limit = 10,
-    sessionoptions = 'buffers,tabpages'
-  }
-}
 local function is_running()
-  vim.cmd('rshada')
-  if vim.fn.exists('g:NVIM_SINGLETON') == 0 then
-    return false
-  end
-  if vim.fn.filereadable(vim.g.NVIM_SINGLETON) == 0 then
-    return false
-  end
-  if vim.g.NVIM_SINGLETON == vim.v.servername then
-    return false
-  end
-  return true
+  return vim.fn.exists('g:NVIM_SINGLETON') ~= 0
+      and vim.fn.filereadable(vim.g.NVIM_SINGLETON) ~= 0
+      and vim.g.NVIM_SINGLETON ~= vim.v.servername
 end
-local function restore_session()
-  local session_dir = M._config.session.dir
-  local session = vim.fs.joinpath(session_dir, vim.fn.getpid() .. '.vim')
-  local files = vim.fn.glob(session_dir .. '/*.vim', false, true)
-  if #files > M._config.session.save_limit then
-    for _, file in ipairs(files) do vim.fn.delete(file) end
-  end
-  vim.fn.mkdir(session_dir, 'p')
-  vim.cmd('mksession! ' .. session .. ' | enew')
+local function restore()
+  vim.cmd('wshada')
   vim.api.nvim_buf_set_lines(vim.api.nvim_get_current_buf(), 0, -1, false, {
-    'target server: ' .. vim.g.NVIM_SINGLETON,
-    'mksession: ' .. session,
+    'server: ' .. vim.g.NVIM_SINGLETON,
     'please wait...'
   })
-  vim.system({ vim.v.progpath, '--server', vim.g.NVIM_SINGLETON, '--remote-send', '<Esc>:source ' .. session .. '<CR>' },
+  vim.system({ vim.v.progpath, '--server', vim.g.NVIM_SINGLETON, '--remote-send', '<Esc>:rshada | normal! `0<CR>' },
     nil, function() vim.schedule(function() vim.cmd('qa!') end) end)
 end
 local function open_other_server()
   vim.opt.swapfile = false
   vim.opt.writebackup = false
   vim.opt.loadplugins = false
-  vim.opt.shada = ''
-  vim.opt.sessionoptions = M._config.session.sessionoptions
-  vim.api.nvim_create_autocmd('VimEnter', { once = true, callback = function() restore_session() end })
+  vim.opt.shadafile = ''
+  vim.api.nvim_create_autocmd('VimEnter', { once = true, callback = restore })
 end
-function M.setup(config)
-  M._config = vim.tbl_deep_extend('force', M._config, config or {})
+function M.setup()
+  vim.cmd('rshada')
   if is_running() then
     open_other_server()
     return true
@@ -54,4 +30,5 @@ function M.setup(config)
   vim.cmd('wshada')
   return false
 end
+
 return M
