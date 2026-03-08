a3d6dc9e794575d913348787af06638683cecb28
nagaohiroki <testplayers0226@gmail.com>
2026-03-09 08:28:06 +0900

    add wezterm

--- nvim/python3/setup.py
+++ nvim/python3/setup.py
@@ -3,22 +3,23 @@ import platform
 import pathlib
 
 
-def make_symlink():
-    nvim = "nvim"
-    src = pathlib.Path.home().joinpath("dotfiles", nvim)
-    if platform.system() == "Windows":
-        symlink(src, pathlib.Path(os.environ["LOCALAPPDATA"], nvim))
+def make_symlink(dir: str, is_xdg: bool):
+    src = pathlib.Path.home().joinpath("dotfiles", dir)
+    if is_xdg:
+        symlink(src, pathlib.Path(os.environ["LOCALAPPDATA"], dir))
     else:
-        symlink(src, pathlib.Path.home().joinpath(".config", nvim))
+        symlink(src, pathlib.Path.home().joinpath(".config", dir))
 
 
 def symlink(src: pathlib.Path, dst: pathlib.Path):
     if dst.exists():
         os.remove(dst)
         print(f"remove {dst}")
+    dst.parent.mkdir(parents=True, exist_ok=True)
     os.symlink(src, dst, target_is_directory=True)
     print(f"symlink {src} -> {dst}")
 
 
 if __name__ == "__main__":
-    make_symlink()
+    make_symlink("nvim", platform.system() == "Windows")
+    make_symlink("wezterm", False)
---
+++ wezterm/wezterm.lua
@@ -0,0 +1,34 @@
+﻿local wezterm = require 'wezterm'
+local config = {}
+config = wezterm.config_builder()
+config.use_ime = true
+config.enable_scroll_bar = true
+config.font = wezterm.font_with_fallback({ [[HackGen Console NF]] })
+config.font_size = 12
+config.hide_tab_bar_if_only_one_tab = true
+config.text_background_opacity = 0.8
+config.window_background_opacity = 0.8
+config.window_frame =
+{
+	inactive_titlebar_bg = 'none',
+	active_titlebar_bg = 'none',
+}
+config.window_padding =
+{
+	top = 0,
+	right = 0,
+	bottom = 0,
+	left = 0
+}
+config.background =
+{
+	{
+		source = { File = 'C:/work/images/image1.jpg' },
+		hsb = { brightness = 0.1 },
+		opacity = 0.8,
+	},
+}
+config.window_close_confirmation = 'NeverPrompt'
+config.initial_cols = 90
+config.initial_rows = 60
+return config
