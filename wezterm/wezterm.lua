ef7fe4d642aa43efebbacafca92fafc38aa7783f
nagaohiroki <testplayers0226@gmail.com>
2026-03-09 09:06:59 +0900

    update wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,10 +1,21 @@
 local wezterm = require 'wezterm'
+local function is_windows()
+  return wezterm.target_triple == 'x86_64-pc-windows-msvc'
+end
+local function is_mac()
+  return wezterm.target_triple == 'aarch64-apple-darwin'
+end
 local config = {}
 config = wezterm.config_builder()
 config.use_ime = true
 config.enable_scroll_bar = true
 config.font = wezterm.font_with_fallback({ [[HackGen Console NF]] })
-config.font_size = 12
+if is_windows() then
+  config.font_size = 12
+end
+if is_mac() then
+  config.font_size = 14
+end
 config.hide_tab_bar_if_only_one_tab = true
 config.text_background_opacity = 0.8
 config.window_background_opacity = 0.8
