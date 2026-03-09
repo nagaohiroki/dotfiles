7ef6690bd81e66cf30b12094ecad0efcfb161475
nagaohiroki <testplayers0226@gmail.com>
2026-03-09 11:10:09 +0900

    add option to resize window

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -16,6 +16,7 @@ end
 if is_mac() then
   config.font_size = 14
 end
+config.adjust_window_size_when_changing_font_size = false
 config.hide_tab_bar_if_only_one_tab = true
 config.text_background_opacity = 0.8
 config.window_background_opacity = 0.8
