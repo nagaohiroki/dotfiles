0ba5e11cd3788bd85180e19b4b2d4de74f829aeb
nagaohiroki <testplayers0226@gmail.com>
2026-03-16 19:49:42 +0900

    add install_app.sh

--- install_app.sh
+++ install_app.sh
@@ -1,3 +1,9 @@
 #!/bin/zsh
+brew install neovim
+brew install uv
+brew install starship
+brew install git
+brew install ripgrep
+brew install --cask wezterm
 uv tool install basedpyright
 uv tool install ruff
