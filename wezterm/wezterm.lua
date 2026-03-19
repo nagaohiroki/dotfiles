6ffd455f475b210d0fe606fc21bf6385736c6927
nagaohiroki <testplayers0226@gmail.com>
2026-03-19 21:03:30 +0900

    delete unused options

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -17,7 +17,6 @@ end
 if is_mac() then
   config.font_size = 14
 end
-config.prefer_to_spawn_tabs = true
 config.adjust_window_size_when_changing_font_size = false
 config.hide_tab_bar_if_only_one_tab = true
 config.text_background_opacity = 0.5
