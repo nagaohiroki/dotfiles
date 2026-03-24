1dea00dddbe0822ab774e7acd3c586926491b603
nagaohiroki <testplayers0226@gmail.com>
2026-03-25 08:32:33 +0900

    add blender and krita

--- install_app.sh
+++ install_app.sh
@@ -5,5 +5,7 @@ brew install starship
 brew install git
 brew install ripgrep
 brew install --cask wezterm
+brew install --cask blender
+brew install --cask krita
 uv tool install basedpyright
 uv tool install ruff
