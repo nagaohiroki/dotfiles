bf3070232728ba2459d8b22a9f79e32ee7e77e81
nagaohiroki <testplayers0226@gmail.com>
2026-03-30 09:29:15 +0900

    change plugins.lua

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -15,7 +15,7 @@ return {
     'tpope/vim-fugitive',
     config = function() vim.keymap.set('n', '<leader>d', ':Gvdiffsplit<CR>') end
   },
-  'tyru/open-browser-github.vim',
+  { 'tyru/open-browser-github.vim', lazy = true },
   {
     'tyru/open-browser.vim',
     config = function() vim.keymap.set('n', '<leader>o', '<Plug>(openbrowser-smart-search)') end
@@ -31,10 +31,10 @@ return {
     ft = { 'markdown' },
     opts = { completions = { lsp = { enabled = true } } }
   },
-  { 'j-hui/fidget.nvim',           opts = {} },
-  { 'uga-rosa/translate.nvim',     opts = {} },
-  { 'nvim-lua/plenary.nvim',       lazy = true },
-  { 'nvim-tree/nvim-web-devicons', opts = {},  lazy = true },
+  { 'j-hui/fidget.nvim',            opts = {} },
+  { 'uga-rosa/translate.nvim',      opts = {} },
+  { 'nvim-lua/plenary.nvim',        lazy = true },
+  { 'nvim-tree/nvim-web-devicons',  opts = {},  lazy = true },
   {
     'folke/tokyonight.nvim',
     lazy = false,
