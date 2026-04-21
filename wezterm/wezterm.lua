a6517fa58e44636bacb7c823df7192bccfdbee75
nagaohiroki <testplayers0226@gmail.com>
2026-04-21 17:06:08 +0900

    delete unused options

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,23 +1,18 @@
 local wezterm = require 'wezterm'
 local config = {}
 config = wezterm.config_builder()
-local dot_dir = wezterm.home_dir .. '/dotfiles'
-config.set_environment_variables =
-{
-  STARSHIP_CONFIG = dot_dir .. '/starship.toml',
-  XDG_CONFIG_HOME = dot_dir
-}
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
   config.font_size = 12
   config.win32_system_backdrop = 'Acrylic'
   config.default_prog = { 'nu' }
 end
-if wezterm.target_triple == 'aarch64-apple-darwin' then
+if wezterm.target_triple == 'aarch64-apple-darwin' or wezterm.target_triple == 'x86_64-apple-darwin' then
   config.font_size = 14
-  config.macos_forward_to_ime_modifier_mask = 'SHIFT|CTRL'
   config.macos_window_background_blur = 20
+  config.macos_forward_to_ime_modifier_mask = 'SHIFT|CTRL'
   config.default_prog = { '/opt/homebrew/bin/nu' }
 end
+config.set_environment_variables = { XDG_CONFIG_HOME = wezterm.home_dir .. '/dotfiles' }
 config.use_ime = true
 config.enable_scroll_bar = true
 config.font = wezterm.font_with_fallback({ [[HackGen Console NF]] })
@@ -26,18 +21,8 @@ config.hide_tab_bar_if_only_one_tab = true
 config.text_background_opacity = 0.5
 config.window_background_opacity = 0.5
 config.color_scheme = 'Tokyo Night'
-config.window_frame =
-{
-  inactive_titlebar_bg = 'none',
-  active_titlebar_bg = 'none',
-}
-config.window_padding =
-{
-  top = 0,
-  right = 0,
-  bottom = 0,
-  left = 0
-}
+config.window_frame = { inactive_titlebar_bg = 'none', active_titlebar_bg = 'none', }
+config.window_padding = { top = 0, right = 0, bottom = 0, left = 0 }
 config.window_close_confirmation = 'NeverPrompt'
 config.initial_cols = 90
 config.initial_rows = 60
