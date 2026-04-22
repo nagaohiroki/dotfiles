c1ab6d949eaada42b07d489dcd525366c3e58b23
nagaohiroki <testplayers0226@gmail.com>
2026-04-22 16:21:04 +0900

    delete unused files

--- .zshrc
+++
@@ -1,3 +0,0 @@
-#!/bin/zsh
-eval "$(starship init zsh)"
-eval "$(zoxide init zsh)"
--- Microsoft.PowerShell_profile.ps1
+++
@@ -1,3 +0,0 @@
-import-Module PSReadLine
-Invoke-Expression (&'starship' init powershell)
-Invoke-Expression (& { (zoxide init powershell | Out-String) })
