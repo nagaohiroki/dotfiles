1b7cc2d4d5b659dd09d78bd95cbd57114d7cb3a7
nagaohiroki <testplayers0226@gmail.com>
2026-04-22 14:21:16 +0900

    add zshenv

--- .zshenv
+++ .zshenv
@@ -1,2 +1,3 @@
 eval "$(/opt/homebrew/bin/brew shellenv)"
 export PATH=$PATH:~/.local/bin
+export XDG_CONFIG_HOME=~/dotfiles
--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 ln -s ~/dotfiles/wezterm ~/.config/wezterm
-ln -s ~/dotfiles/.zprofile ~/.zprofile
+ln -s ~/dotfiles/.zshenv ~/.zshenv
 brew install neovim
 brew install uv
 brew install starship
