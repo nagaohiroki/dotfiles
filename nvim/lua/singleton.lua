4f57c91a5100af9ffe6a0229218711c20c370587
nagaohiroki <testplayers0226@gmail.com>
2026-04-24 12:07:29 +0900

    add progress_screen

--- nvim/lua/singleton.lua
+++ nvim/lua/singleton.lua
@@ -1,12 +1,34 @@
 local M = {}
+local function progress_screen()
+  local frames = { '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏' }
+  local i = 1
+  local timer = vim.uv.new_timer()
+  if timer == nil then return end
+  timer:start(0, 100, vim.schedule_wrap(function()
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, {
+      'server: ' .. vim.g.NVIM_SINGLETON,
+      'please wait... ' .. frames[i] })
+    i = i % #frames + 1
+  end))
+  return timer
+end
 local function open_buffer()
+  local timer = progress_screen()
+  if timer == nil then return end
+  local function on_exit()
+    vim.schedule(function()
+      timer:stop()
+      timer:close()
+      vim.cmd('qa!')
+    end)
+  end
   vim.cmd('wshada')
-  vim.api.nvim_buf_set_lines(0, 0, -1, false, { 'server: ' .. vim.g.NVIM_SINGLETON, 'please wait...' })
   local open = '<Esc>:rshada | normal! `0<CR>'
   if vim.api.nvim_buf_get_name(0) == '' then open = '<Esc>:enew<CR>' end
   local cmd = { vim.v.progpath, '--server', vim.g.NVIM_SINGLETON, '--remote-send', open }
-  vim.system(cmd, nil, function() vim.schedule(function() vim.cmd('qa!') end) end)
+  vim.system(cmd, nil, on_exit)
 end
+
 function M.setup()
   vim.cmd('silent! rshada')
   if vim.fn.filereadable(vim.g.NVIM_SINGLETON) == 0 or vim.g.NVIM_SINGLETON == vim.v.servername then
