7705b4b5922ab904b8ce449f53f59e584f1c0649
nagaohiroki <testplayers0226@gmail.com>
2026-04-24 11:13:31 +0900

    singleton update

--- nvim/lua/singleton.lua
+++ nvim/lua/singleton.lua
@@ -1,4 +1,12 @@
 local M = {}
+local function open_buffer()
+  vim.cmd('wshada')
+  vim.api.nvim_buf_set_lines(0, 0, -1, false, { 'server: ' .. vim.g.NVIM_SINGLETON, 'please wait...' })
+  local open = '<Esc>:rshada | normal! `0<CR>'
+  if vim.api.nvim_buf_get_name(0) == '' then open = '<Esc>:enew<CR>' end
+  local cmd = { vim.v.progpath, '--server', vim.g.NVIM_SINGLETON, '--remote-send', open }
+  vim.system(cmd, nil, function() vim.schedule(function() vim.cmd('qa!') end) end)
+end
 function M.setup()
   vim.cmd('silent! rshada')
   if vim.fn.filereadable(vim.g.NVIM_SINGLETON) == 0 or vim.g.NVIM_SINGLETON == vim.v.servername then
@@ -9,17 +17,7 @@ function M.setup()
   vim.opt.swapfile = false
   vim.opt.writebackup = false
   vim.opt.shadafile = ''
-  vim.api.nvim_create_autocmd('VimEnter', {
-    once = true,
-    callback = function()
-      vim.cmd('wshada')
-      vim.api.nvim_buf_set_lines(0, 0, -1, false, { 'server: ' .. vim.g.NVIM_SINGLETON, 'please wait...' })
-      local open = '<Esc>:rshada | normal! `0<CR>'
-      if vim.api.nvim_buf_get_name(0) == '' then open = '<Esc>:enew<CR>' end
-      local cmd = { vim.v.progpath, '--server', vim.g.NVIM_SINGLETON, '--remote-send', open }
-      vim.system(cmd, nil, function() vim.schedule(function() vim.cmd('qa!') end) end)
-    end
-  })
+  vim.api.nvim_create_autocmd('VimEnter', { callback = open_buffer })
   return true
 end
 
