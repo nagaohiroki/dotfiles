a8bbb0a91a97494883eb9431c626ae2aecc82cbe
nagaohiroki <testplayers0226@gmail.com>
2026-05-06 16:31:50 +0900

    add install_linux

---
+++ .bashrc
@@ -0,0 +1,3 @@
+eval "$($HOME/.local/bin/mise activate bash)"
+export XDG_CONFIG_HOME=~/dotfiles
+export PATH=$PATH:~/.local/bin
---
+++ install_linux.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mkdir -p ~/.config
+ln -sf ~/dotfiles/wezterm ~/.config/wezterm
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
